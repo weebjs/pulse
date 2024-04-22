b37591c997a2bac03dfaafdd6f5a0a18c2759336
weeb .js <ilyoreo99@gmail.com>
2024-04-22 01:31:00 +0000

    changed a few things.

--- handlers/command.js
+++ handlers/command.js
@@ -5,11 +5,11 @@ let table = new ascii("Commands");
 table.setHeading("Command", "Load status");
 
 module.exports = (client) => {
-  readdirSync("./commands").forEach(dir => {
-    const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
+  readdirSync("./src").forEach(dir => {
+    const commands = readdirSync(`./src/${dir}/`).filter(file => file.endsWith(".js"));
 
     for (let file of commands) {
-      let pull = require(`../commands/${dir}/${file}`);
+      let pull = require(`../src/${dir}/${file}`);
       pull.name = file.replace(".js", "")
       pull.category = dir
       client.commands.set(pull.name, pull);
